3d6631d428fdb5bbf21d620e79ab0d5572cafe62
Subodh-112 <subodhchauhan8880@gmail.com>
2023-10-18 17:45:24 +0530

    whatspp images 18

--- src/screen/ticket/widgets/whatsapp/PatientReply.tsx
+++ src/screen/ticket/widgets/whatsapp/PatientReply.tsx
@@ -2,14 +2,19 @@ import { Box, Typography } from '@mui/material';
 import dayjs from 'dayjs';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { image } from 'pdfkit/js/mixins/images';
 
 type Props = {
   message: any;
 };
 
 const PatientReply = ({ message }: Props) => {
-  const [imageBlob, setImageBlob] = useState(null);
+ 
+ const [imageUrl, setImageUrl] = useState('');
+ 
+
+
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -23,36 +28,19 @@ const PatientReply = ({ message }: Props) => {
             }
           }
         );
-
-        // Assuming response.data.url is the URL of the image
-        const imageurl = JSON.stringify(response.data?.url);
-        const myHeaders = new Headers();
-        myHeaders.append(
-          'Authorization',
-          'Bearer EAALU5Uh1hCoBAHOvIZAOLuJVrUltYe3uMCIQwKvayQCZC5zR45RO9iK5ZAeRNUKhZB3dShZBM4DugqeUtw9ZCIYOr39g3fqGsjYYycjNPb4CpMFZCQY4rqUSXaPHHam8utfUUzC4NBBSYLkoZCuSEW1oPl6TaZCK7hgmJ1h1E5DxXw8BEXKW1Vs2P'
-        );
-
-        const requestOptions = {
-          method: 'GET',
-          headers: myHeaders,
-          redirect: 'follow' as RequestRedirect
-        };
-
-        fetch(
-          imageurl,
-          requestOptions
-        )
-          .then((response) => response.text())
-          .then((result) => {
-            // Handle the result here
-            console.log(result);
-          })
-          .catch((error) => {
-            console.error('Error:', error);
+ const image = JSON.stringify(response.data?.url);
+ console.log(image)
+        // Handle the response data here
+          const imageResponse = await axios.get(image, {
+            responseType: 'blob',
+            headers: {
+              Authorization:
+                'Bearer EAALU5Uh1hCoBAHOvIZAOLuJVrUltYe3uMCIQwKvayQCZC5zR45RO9iK5ZAeRNUKhZB3dShZBM4DugqeUtw9ZCIYOr39g3fqGsjYYycjNPb4CpMFZCQY4rqUSXaPHHam8utfUUzC4NBBSYLkoZCuSEW1oPl6TaZCK7hgmJ1h1E5DxXw8BEXKW1Vs2P'
+            } // Specify 'blob' for binary data like images
           });
 
-        // Handle the response data here
-      
+ setImageUrl(URL.createObjectURL(imageResponse.data));
+
       } catch (error) {
         // Handle any errors that occurred during the request
         console.error(error);
@@ -62,29 +50,11 @@ const PatientReply = ({ message }: Props) => {
     fetchData();
   }, []);
 
-  console.log(imageBlob);
-
-  // Function to trigger the download
-  const downloadImage = () => {
-    if (imageBlob) {
-      // Create a blob URL for the image
-      const url = URL.createObjectURL(imageBlob);
-
-      // Create an anchor element with the download attribute
-      const link = document.createElement('a');
-      link.href = URL.createObjectURL(imageBlob);
-      link.download = 'media_file.jpg'; // Specify the desired file name
-
-      // Trigger a click event to download the file
-      link.click();
-
-      // Revoke the object URL to free up resources
-      URL.revokeObjectURL(url);
-    }
-  };
-
-
   
+
+console.log(imageUrl)
+
+
 
 
   return (
@@ -99,15 +69,7 @@ const PatientReply = ({ message }: Props) => {
       {message.text ? (
         <Typography>{message.text}</Typography>
       ) : (
-        <p>text not found</p>
-      )}
-      {imageBlob ? (
-        <div>
-          <img src={URL.createObjectURL(imageBlob)} alt="Image" />
-          <button onClick={downloadImage}>Download Image</button>
-        </div>
-      ) : (
-        <p>Image not found</p>
+       <img src={imageUrl} alt="Image" />
       )}
 
       <Box display="flex" justifyContent="flex-start">

a6be3504591105fd5f126dec7eb26eabf954fdaa
Subodh-112 <subodhchauhan8880@gmail.com>
2023-10-19 14:59:16 +0530

    whatspp images 22

--- src/screen/ticket/widgets/whatsapp/PatientReply.tsx
+++ src/screen/ticket/widgets/whatsapp/PatientReply.tsx
@@ -10,24 +10,9 @@ type Props = {
 
 const PatientReply = ({ message }: Props) => {
  
-   const isImage = message.url.match(/\.(jpeg|jpg|gif|png|webp)$/);
+ 
 
-  const handleImageClick = () => {
-    if (isImage) {
-      // Create a hidden anchor element
-      const downloadLink = document.createElement('a');
-      downloadLink.href = message.url; // Set the URL to the image
-      downloadLink.download = 'image.jpg'; // Specify the default file name for the downloaded image
-      downloadLink.style.display = 'none';
 
-      // Trigger a click event on the anchor element
-      document.body.appendChild(downloadLink);
-      downloadLink.click();
-
-      // Clean up
-      document.body.removeChild(downloadLink);
-    }
-  };
 
 
 
@@ -36,26 +21,20 @@ const PatientReply = ({ message }: Props) => {
 
   return (
     <Box
-      style={{
-        boxShadow: '0 1px .5px rgba(11, 20, 26, .13)',
-        margin: '1rem 0',
-        maxWidth: '70%',
-        padding: '1rem',
-        backgroundColor: '#f5f5f5',
-        borderRadius: '7.5px 7.5px 7.5px 0px',
-        cursor: isImage ? 'pointer' : 'default' // Add a pointer cursor to indicate clickability for images
-      }}
-      onClick={handleImageClick}
+      boxShadow=" 0 1px .5px rgba(11,20,26,.13)"
+      my={1}
+      maxWidth="70%"
+      p={1}
+      bgcolor="#f5f5f5"
+      borderRadius="7.5px 7.5px 7.5px 0px"
+      
     >
       {message.text ? (
-        <p>{message.text}</p>
-      ) : isImage ? (
-        <a href={message.url} download="image.jpg">
-          <img src={message.url} alt="Image" style={{ width: '100%' }} />
-        </a>
+        <Typography>{message.text}</Typography>
       ) : (
-        <img src={message.url} alt="Image" style={{ width: '100%' }} />
+       <img src={message.url} alt="Image" />
       )}
+
       <Box display="flex" justifyContent="flex-start">
         <Typography variant="caption" fontSize="0.7rem" color="GrayText">
           {dayjs(message.createdAt).format('DD MMM YYYY hh:mm A')}

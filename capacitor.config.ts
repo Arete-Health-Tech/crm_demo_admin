ced887a7be5e3aca89267475966f2508a001a080
Anoop kumar <anoopkumar@aretehealth.tech>
2024-07-04 14:38:37 +0530

    app changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'IVY-CRM',
-  webDir: 'build'
+  webDir: 'build',
+  server: {
+    url: "https://arete.demo.aretehealth.tech/prod/api/v1/"
+    // url: "http://localhost:3000/prod/api/v1/"
+  }
 };
 
 export default config;

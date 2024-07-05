10c1acc677936ffe895f588637d2f9dc9dcabee5
macbook pro <subodhchauhan8880@gmail.com>
2024-07-05 16:05:08 +0530

    update in socket for message2

--- src/screen/ticket/widgets/whatsapp/ExpandedModal.tsx
+++ src/screen/ticket/widgets/whatsapp/ExpandedModal.tsx
@@ -2,9 +2,11 @@ import { Modal } from '@mui/material'
 import React from 'react'
 import useTicketStore from '../../../../store/ticketStore';
 import MessagingWidget from './WhatsappWidget';
+import { useParams } from 'react-router-dom';
 
 const ExpandedModal = () => {
     const { setWhtsappExpanded, whtsappExpanded } = useTicketStore();
+    const {ticketID}=useParams();
 
     return (
         <>
@@ -14,7 +16,7 @@ const ExpandedModal = () => {
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
             >
-                <MessagingWidget />
+                <MessagingWidget ticketId={ticketID}/>
             </Modal>
         </>
     )

2e744608d4bec86142fa2c2e26146be98af24eaf
macbook pro <subodhchauhan8880@gmail.com>
2024-07-30 17:34:43 +0530

    all

--- src/container/layout/TicketLayout.tsx
+++ src/container/layout/TicketLayout.tsx
@@ -802,7 +802,7 @@ const Ticket = () => {
                       boxShadow="0px 0px 10px rgba(0,0,0,0.1)"
                     >
                       <Stack className="ticket-asssignee-container-layout">
-                      <MenuItem sx={menuItemStyles} onClick={() => (setVisible(false), localStorage.setItem('location', " "))}>
+                      <MenuItem sx={menuItemStyles} onClick={() => (setVisible(false), localStorage.setItem('location', ""))}>
                           All 
                         </MenuItem>
                         <MenuItem sx={menuItemStyles} onClick={() => (setVisible(false), localStorage.setItem('location', "Mohali"))}>

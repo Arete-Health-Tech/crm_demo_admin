7cbc590ab2bc3a7433241d9f7bfad46144a68de8
mr-clover001 <anoopkumar08@github.com>
2025-05-26 12:47:45 +0530

    changes

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -15,7 +15,7 @@ export const SERVER_URL = 'https://arete.demo.aretehealth.tech/prod/api/v1/';
 // export const BACKEND_URL = 'http://localhost:5050';
 
 export const BACKEND_URL = 'https://arete.demo.aretehealth.tech';
-// console.log("hello")
+console.log("hello")
 
 const headers: Readonly<Record<string, string | boolean>> = {
   Accept: 'application/json',
